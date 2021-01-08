da2471b51b2127cf20b49226cf269e54dea7eb3f
timframe <t.f.retouching@gmail.com>
2021-01-08 12:43:59 +0000

    create job and get single job route created

--- index.js
+++ index.js
@@ -3,9 +3,12 @@ import mongoose from 'mongoose'
 import Job from './models/job.js'
 
 
+
 const dbUri = 'mongodb://localhost/deverr-db'
 const app = express()
 const port = 4000
+
+app.use(express.json())
 
 async function startServer(){
   try {
@@ -27,9 +30,34 @@ app.use((req, res, next ) => {
   next()
 })
 
+//*GET ALL JOBS
 app.get('/jobs', async (req, res) => {
   const jobs = await Job.find()
   return res.status(200).json(jobs)
+})
+
+//*POST JOB
+app.post('/jobs', async(req, res) => {
+  try {
+    const newJob = await Job.create(req.body)
+    return res.status(201).json(newJob)
+  } catch (err){
+    console.log(err)
+    return res.status(422).json(err)
+  }
+})
+
+//*GET SINGLE JOB
+app.get('jobs/:id', async(req, res) => {
+  const { id } = req.params
+  try {
+    const job = await Job.findById(id)
+    if (!job) throw new Error()
+    return res.status(200).json(job)
+  } catch (err) {
+    console.log(err)
+    return res.status(404).json({ 'message': 'Not Found' })
+  }
 })
 
 
