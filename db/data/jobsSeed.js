beb6b0af8c7dbc623b732760663f9a733995411b
Sandra Spighel <s.spighel@gmail.com>
2021-01-08 18:20:51 +0000

    DATA-JOB-SEEDS-Added

---
+++ db/data/jobsSeed.js
@@ -0,0 +1,26 @@
+export default [{
+  jobTitle: 'Android Developer for Game Commission (6 month contract)',
+  jobDescription: 'Android Developer required for designing and developing match three type game for the Android platform. Required: Experience with unit-testing code for robustness, including edge cases, usability, and general reliability; bug fixing and improving application performance',
+  jobDeadline: '05 May 2021',
+  jobPhoto: 'https://www.technoworldnews.com/wp-content/uploads/2017/03/mobile-app-developer.jpg',
+  jobCategory: 'Android-Dev',
+  jobFee: 15000,
+  jobIsLive: true,
+}, {
+  jobTitle: 'UI Designer required for 3 month project',
+  jobDescription: 'Looking for an experienced Product designer with a passion for creating intuitive and delightful user experiences. Opportunity to join a fast-growing and highly visible team. 1+ years of design experience with a strong portfolio showcasing complex platforms turned into engaging user interfaces',
+  jobDeadline: '04 May 2021',
+  jobPhoto: 'https://cdn.wccftech.com/wp-content/uploads/2016/10/Front-End-Development-Bundle.jpg',
+  jobCategory: 'UI-Dev',
+  jobFee: 6000,
+  jobIsLive: true,
+}, {
+  jobTitle: 'Junior Front-end Developer – Angular (Contract)',
+  jobDescription: 'POSITION: Contract until March 2022; COMMENCEMENT: January 2021; REQUIREMENTS: 3+ years of IT / Engineering Degree/relevant experience; Remote work, Willing and able to travel 3 times a year for up to 2 weeks to Munich (Germany)',
+  jobDeadline: '07 Feb 2021',
+  jobPhoto: 'https://frontendmasters.com/books/front-end-handbook/2017/images/spectrum.png',
+  jobCategory: 'Front-End',
+  jobFee: 4500,
+  jobIsLive: true,
+}, {
+}]
