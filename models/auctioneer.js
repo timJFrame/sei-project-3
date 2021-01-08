6a1d588c680c49209e6bbc85d9d138f53afd47f2
Sandra Spighel <s.spighel@gmail.com>
2021-01-08 15:54:23 +0000

    MODELS-AUCTIONEER-Updating-model All models updated with fields uncommented

--- models/auctioneer.js
+++ models/auctioneer.js
@@ -4,9 +4,4 @@ const auctioneerSchema = new mongoose.Schema({
   user: { type: mongoose.Schema.ObjectId, ref: 'User', required: true },
 })
 
-// auctioneerSchema.virtual('passwordConfirmation')
-//   .set(function(passwordConfirmation) {
-//     this._passwordConfirmation = passwordConfirmation
-//   })
-
 export default mongoose.model('Auctioneer', auctioneerSchema)
