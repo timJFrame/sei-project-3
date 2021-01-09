b5d9869afeed2faf1c679b7db314aa711a51f7cd
Sandra Spighel <s.spighel@gmail.com>
2021-01-09 09:20:42 +0000

    Commented out bids and comments temporary to help with testing

--- db/seed.js
+++ db/seed.js
@@ -1,8 +1,10 @@
 import mongoose from 'mongoose'
 import connectToDatabase from '../lib/connectToDB.js'
-import auctioneersSeed from './data/auctioneersSeed.js'
-import Auctioneer from '../models/auctioneer.js'
-
+// import auctioneersSeed from './data/auctioneersSeed.js'
+// import Auctioneer from '../models/auctioneer.js'
+import Job from '../models/job.js'
+import jobsData from '../db/data/jobsSeed.js'
+import users from '../models/user.js'
 
 async function seedDatabase() {
   try {
@@ -14,11 +16,19 @@ async function seedDatabase() {
 
     console.log('🤖 Database dropped')
 
-    const auctioneers = auctioneersSeed()
+    // const auctioneers = auctioneersSeed()
 
-    const createdAuctioneers = await Auctioneer.create(auctioneers) // ! then pass that users array
+    // const createdAuctioneers = await Auctioneer.create(auctioneers) // ! then pass that users array
 
-    console.log(`😎 Created ${createdAuctioneers.length}`)
+    // console.log(`😎 Created ${createdAuctioneers.length}`)
+
+    const jobDataWithOwners = jobsData.map(job => {
+      job.owner = users[0]._id
+      return job
+    })
+
+    const jobs = await Job.create(jobDataWithOwners)
+    console.log(`POW! Fresh Database containing ${jobs.length} jobs`)
 
     await mongoose.connection.close()
 
--- models/job.js
+++ models/job.js
@@ -2,21 +2,21 @@ import mongoose from 'mongoose'
 import uniqueValidator from 'mongoose-unique-validator'
 
 
-const jobBidsSchema = new mongoose.Schema({
-  text: { type: String, required: true, maxlength: 300 },
-  owner: { type: mongoose.Schema.ObjectId, ref: 'Bidder', required: true },
-}, {
-  timestamps: true,
-})
+// const jobBidsSchema = new mongoose.Schema({
+//   text: { type: String, required: true, maxlength: 300 },
+//   owner: { type: mongoose.Schema.ObjectId, ref: 'Bidder', required: true },
+// }, {
+//   timestamps: true,
+// })
 
-const jobCommentsSchema = new mongoose.Schema({
-  text: { type: String, required: true, maxlength: 300 },
-  rating: { type: Number, required: true, min: 1, max: 5 },
-  isAuctioneer: { type: mongoose.Schema.ObjectId, ref: 'Auctioneer', default: null },
-  isBidder: { type: mongoose.Schema.ObjectId, ref: 'Bidder', default: null },
-}, {
-  timestamps: true,
-})
+// const jobCommentsSchema = new mongoose.Schema({
+//   text: { type: String, required: true, maxlength: 300 },
+//   rating: { type: Number, required: true, min: 1, max: 5 },
+//   isAuctioneer: { type: mongoose.Schema.ObjectId, ref: 'Auctioneer', default: null },
+//   isBidder: { type: mongoose.Schema.ObjectId, ref: 'Bidder', default: null },
+// }, {
+//   timestamps: true,
+// })
 
 const jobSchema = new mongoose.Schema({
   jobTitle: { type: String, required: true },
@@ -26,9 +26,9 @@ const jobSchema = new mongoose.Schema({
   jobCategory: { type: String, required: true },
   jobFee: { type: Number, required: true, min: 1 },
   jobIsLive: { type: Boolean, required: true },
-  jobOwner: { type: mongoose.Schema.ObjectId, ref: 'Auctioneer', required: true },
-  jobBids: [jobBidsSchema],
-  jobComments: [jobCommentsSchema],
+  // jobOwner: { type: mongoose.Schema.ObjectId, ref: 'Auctioneer', required: true },
+  // jobBids: [jobBidsSchema],
+  // jobComments: [jobCommentsSchema],
 })
 
 jobSchema.plugin(uniqueValidator)
