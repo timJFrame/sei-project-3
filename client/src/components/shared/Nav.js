6d749d58d28ebf1822352e875002009bd1b2f527
timframe <t.f.retouching@gmail.com>
2021-01-11 18:21:13 +0000

    merge conflict

--- client/src/components/shared/Nav.js
+++ client/src/components/shared/Nav.js
@@ -11,8 +11,8 @@ function Nav() {
         </div>
         <div className="navButtonsGroup">
           <Link to="/jobs" className="btn">Jobs</Link>
-          <Link to="#" className="btn-secondary-xs">Login</Link>
-          <Link to="#" className="btn-submit-lg">Register</Link>
+          <Link to="/login" className="btn-secondary-xs">Login</Link>
+          <Link to="/register" className="btn-submit-lg">Register</Link>
           <Link to="#" className="btn-cancel-xl">Delete</Link>
         </div>
 
