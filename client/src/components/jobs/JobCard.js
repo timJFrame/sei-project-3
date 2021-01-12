a7b2081efa7c6e5dbb5c8ec28bf523c667bc0086
Alberto <cerrone.alberto93@gmail.com>
2021-01-12 16:48:57 +0100

    working on card links

--- client/src/components/jobs/JobCard.js
+++ client/src/components/jobs/JobCard.js
@@ -3,37 +3,41 @@ import { Link } from 'react-router-dom'
 
 function JobCard({ _id, jobTitle, jobPhoto }) {
   return (
-    <Link to={`/jobs/${_id}`}>
-      <div className="job-card glass-morphism">
-        <div className="card-image-container">
+
+    <div className="job-card glass-morphism">
+      <div className="card-image-container">
+        <Link to={`/jobs/${_id}`}>
           <img src={jobPhoto} alt={jobTitle} className="card-image" />
+        </Link>
+      </div>
+      <div className="card-body">
+        <div className="card-body-user">
+          <div className="">
+            <img src={jobPhoto} alt={jobTitle} className="card-user-avatar" />
+          </div>
+          <div className="card-user-name">
+            <p className="">Auctioneer Name</p>
+          </div>
         </div>
-        <div className="card-body">
-          <div className="card-body-user">
-            <div className="">
-              <img src={jobPhoto} alt={jobTitle} className="card-user-avatar" />
-            </div>
-            <div className="card-user-name">
-              <p className="">Auctioneer Name</p>
-            </div>
-          </div>
-          <div className="card-body-text">
+        <div className="card-body-text">
+          <Link to={`/jobs/${_id}`}>
             <p>{jobTitle}</p>
-          </div>
-          <hr />
-          <div className="card-body-footer-container">
-            <div className="card-footer-body">
-              <div className="card-footer-element">
-                <p>something</p>
-              </div>
-              <div className="card-footer-element">
-                <p>something</p>
-              </div>
+          </Link>
+        </div>
+        <hr />
+        <div className="card-body-footer-container">
+          <div className="card-footer-body">
+            <div className="card-footer-element">
+              <p>something</p>
+            </div>
+            <div className="card-footer-element">
+              <p>something</p>
             </div>
           </div>
         </div>
       </div>
-    </Link>
+    </div>
+    </Link >
   )
 }
 
