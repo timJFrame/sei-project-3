964b3640e78334a98dc9665d833b15bac1fba36c
Sandra Spighel <s.spighel@gmail.com>
2021-01-14 17:27:01 +0000

    FRONT-MERGE-Err Fixed

--- client/src/components/shared/Home.js
+++ client/src/components/shared/Home.js
@@ -6,41 +6,21 @@ function Home() {
 
   return (
     <div className="home-page-container">
-<<<<<<< HEAD
-
       <section className="hero">
         <div className="hero-content">
-=======
-      <div className="hero-image-container">
-        <div className="hero-image">
-          <p>
-            <img src={hero} alt="hero-image" height="700px" />
-          </p>
->>>>>>> development
         </div>
       </section>
       
       <br />
       <div className="job-show-container frosty about-text">
 
-<<<<<<< HEAD
         <h1 className="about-title">DEVERR brings together Freelance Developers and Contractors</h1>
-=======
-      <div className='job-show-container'>
-        <br />
-        <h1>DEVERR brings together Freelance Developers and Contractors.</h1>
->>>>>>> development
         <br />
         <h2 className="about-text">Focus on delivering your project - the team you need is here</h2>
         <br />
         <hr />
         <br />
-<<<<<<< HEAD
         
-=======
-        <h1>STEPS</h1>
-        <br />
->>>>>>> development
         <h2>IF YOU ARE LOOKING FOR A DEV</h2>
         <h4><strong>One</strong> - Register as <strong>Auctioneer</strong></h4>
         <h4><strong>Two</strong> - Post the job</h4>
@@ -53,10 +33,7 @@ function Home() {
         <h4><strong>Two</strong> - Browse all jobs or filter by category</h4>
         <h4><strong>Three</strong> - Post a bid</h4>
       </div>
-<<<<<<< HEAD
       <br />
-=======
->>>>>>> development
 
       <Footer />
     </div>
