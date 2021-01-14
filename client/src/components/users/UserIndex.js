4d9a766b6300d1937bcc8929ac04a167b27842f8
Sandra Spighel <s.spighel@gmail.com>
2021-01-14 18:22:11 +0000

    FRONT-SHOW-ALL-USERS-COMPONENT-Edit

--- client/src/components/users/UserIndex.js
+++ client/src/components/users/UserIndex.js
@@ -1,7 +1,50 @@
 import React from 'react'
+import { getAllUsers } from '../../lib/api'
 
 function UserIndex() {
-  return <h1>User Index</h1>
+  const [users, setUsers] = React.useState(null)
+
+
+  React.useEffect(() => {
+    const getData = async () => {
+      try {
+        const { data } = await getAllUsers()
+        setUsers(data)
+      } catch (err) {
+        console.log(err)
+      }
+    }
+    getData()
+  }, [])
+
+  return (
+    <div className="container-general">
+      <h1>AUCTIONEERS:</h1>
+      <br />
+      {users ?
+        users.map(user => (
+          user.isAuctioneer && 
+          <h1 key={user._id} { ...user }>{user.name} </h1>
+          // <UserCard key={user._id}  {...user} />
+        ))
+        :
+        <h2>Loading Users</h2>
+      }
+      <br />
+      <h1>BIDDERS:</h1>
+      <br />
+      {users ?
+        users.map(user => (
+          !user.isAuctioneer && 
+          <h1 key={user._id} { ...user }>{user.name}</h1>
+          // <UserCard key={user._id}  {...user} />
+        ))
+        :
+        <h2>Loading Users</h2>
+      }
+
+    </div>
+  )
 }
 
 export default UserIndex
